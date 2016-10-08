a8ec715e69eed4187e4583f3b65c442d0503541a
Festus Yeboah <bretuobay@gmail.com>
2016-10-08 02:01:47 +0200

    added test for final obj

--- test/cleanextest.js
+++ test/cleanextest.js
@@ -8,12 +8,51 @@ var cleanex = require('../lib/index');
 describe("#Testing loading for cleanex ", function() {
   describe("should load utility module and call some utility functions ", function() {
     it("it should return true", function() {
-
-      var cleannum = cleanex.cleanNum();
-      console.log(cleannum);
-      assert.equal(cleannum, true, 'error if not null');
+      //var testvar = cleanex.confirm_type(confirm._bool,'true');
+        var testvar = cleanex.confirm_type('_bool',true);
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not boolean');
 
     });
+
+    it("it should return true", function() {
+      //var testvar = cleanex.confirm_type(confirm._bool,'true');
+        var testvar = cleanex.confirm_type('_int',12);
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not int');
+    });
+
+    it("it should return true", function() {
+      //var testvar = cleanex.confirm_type(confirm._bool,'true');
+        var testvar = cleanex.confirm_type('_float',12.2);
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not float');
+    });
+
+    it("it should return true", function() {
+        var testvar = cleanex.confirm_type('_string','hshshs');
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not string');
+    });
+
+    it("it should return true", function() {
+        var testvar = cleanex.confirm_type('_date', new Date());
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not date');
+    });
+
+    it("it should return true", function() {
+        var testvar = cleanex.confirm_type('_object', new Date()); // date is object  and passes
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not object');
+    });
+
+    it("it should return true", function() {
+        var testvar = cleanex.confirm_type('_null',"null"); //
+      console.log(testvar);
+      assert.equal(testvar, true, 'error if not null variable');
+    });
+
   });
 
 });
